e57fac83109a91ddf53eea7152d7b1f099560164
Smi-lin <olajideolabimpe26@gmail.com>
2024-03-04 21:08:41 +0100

    third commit

--- src/components/Account/Login.jsx
+++ src/components/Account/Login.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import {Box, TextField, Button, styled} from "@mui/material"
 
+const component = styled(Box)`width: 400px`;
+
 const Login = () => {
 
     const imageUrl = "https://www.sesta.it/wp-content/uploads/2021/03/logo-blog-sesta-trasparente.png"
