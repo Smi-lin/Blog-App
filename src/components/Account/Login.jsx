6bb27d30a7b21087e2cc3fb978bc428c9e56cfeb
Smi-lin <olajideolabimpe26@gmail.com>
2024-03-04 20:41:43 +0100

    third commit

--- src/components/Account/Login.jsx
+++ src/components/Account/Login.jsx
@@ -3,7 +3,7 @@ import {Box} from "@mui/material"
 
 const Login = () => {
 
-    const imageUrl = "https://www.siesta.it/wp-content/uploads/2021/03/logo-blog-siesta-transparente.png"
+    const imageUrl = "https://www.sesta.it/wp-content/uploads/2021/03/logo-blog-sesta-trasparente.png"
 
   return (
     <Box>
